21e4b22100fd926bf0cfdd50088147cd1b5dc4f6
Harry Ku <harry@Harrys-MBPR.local>
2019-05-08 16:36:58 -0700

    Init project.

---
+++ devops01a.sh
@@ -0,0 +1,9 @@
+# a) in folder DevOps01, create a bash script called devops01a.sh that gets information about your computer including:
+# number of volumes, size of each volume, free space on each volume
+df -h
+# number cpus/cores, information about the cpus/core
+# amount of ram, your mac address and ip address
+
+# b) periodically, stage this file and commit it locally in git
+
+# c) periodically commit this file to the remote GitHub repo
