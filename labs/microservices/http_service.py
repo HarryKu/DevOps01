576ad5adc26f6808d1862aa3ceb2ff25a4edb4ca
Harry Ku <harry@Harrys-MBPR.local>
2019-05-25 19:28:20 -0700

    Add http_service

---
+++ labs/microservices/http_service.py
@@ -0,0 +1,13 @@
+import json
+from nameko.web.handlers import http
+
+class HttpService:
+    name = "http_service"
+
+    @http('GET', '/get/<int:value>')
+    def get_method(self, request, value):
+        return json.dumps({'value-plus-one': value + 1})
+
+    @http('POST', '/post')
+    def do_post(self, request):
+        return u"received: {}".format(request.get_data(as_text=True))
